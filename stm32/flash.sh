2c77afa7c7c49aab75be876878d77ba101e89d8b
Michael Ambrus <michael.ambrus@gmail.com>
2011-04-03 19:09:10 +0200

    Improvements
    Flash stm32 will try to use formatted write of openocd
    src.cgrep.sh will search in linkerscripts as well

--- stm32/flash.sh
+++ stm32/flash.sh
@@ -4,12 +4,26 @@ if [ -z $FLASH_SH ]; then
 
 FLASH_SH="flash.sh"
 
-function flash() {
-	emb.openocd.cmd.exp reset halt
-	emb.openocd.cmd.exp flash probe 0
-	emb.openocd.cmd.exp stm32x mass_erase 0
-	emb.openocd.cmd.exp flash write_bank 0 $1 0
-	emb.openocd.cmd.exp reset halt
+# Preferred flash method (bin). Because its faster and doesnt erase more than it
+# needs to)
+function flash_bin() {
+	echo "Flashing binary"
+	emb.openocd.cmd.exp reset halt						&& \
+	emb.openocd.cmd.exp flash probe 0					&& \
+	emb.openocd.cmd.exp stm32x mass_erase 0				&& \
+	emb.openocd.cmd.exp flash write_bank 0 $1 0			&& \
+	emb.openocd.cmd.exp reset halt						&& \
+	echo "Flashed [$1] OK"
+}
+
+# Generic formatted flash. Lets OpenOCD figure out the format
+function flash_form() {
+	echo "Flashing formatted"
+	emb.openocd.cmd.exp reset halt						&& \
+	emb.openocd.cmd.exp flash probe 0					&& \
+	emb.openocd.cmd.exp flash write_image 1 1 $1 		&& \
+	emb.openocd.cmd.exp reset halt						&& \
+	echo "Flashed [$1] OK"
 }
 
 source s3.ebasename.sh
@@ -20,13 +34,22 @@ if [ "$FLASH_SH" == $( ebasename $0 ) ]; then
 	#set -u
 	source s3.user_response.sh
 
-	BIN=$( (ls *.bin 2>/dev/null; ls *.elf 2>/dev/null) | tail -1 )
+	#Several valid formats may be present. Next line chooses in preferred order
+	IMG=$( (ls *.bin 2>/dev/null; ls *.elf 2>/dev/null) | head -n1)
+	EXT=$(echo $IMG | sed -e 's/\(.*\)\.//')
 
 	if [ $# -ge 1 ]; then
-		emb.openocd.cmd.exp $@
+		#emb.openocd.cmd.exp $@
+		flash_form "$@"
 	else
-		ask_user_continue "Flash target with [${BIN}]? (Y/n)" || exit 1
-		flash $BIN
+		ask_user_continue "Flash target with [${IMG}]? (Y/n)" || exit 1
+		if [ "X${EXT}" == "Xbin" ]; then
+			flash_bin $IMG
+			exit $?
+		else
+			flash_form $IMG
+			exit $?
+		fi
 	fi
 fi
 
