23b60d5644cd4a9b0fbaade244b685b6456e5ad0
Michael Ambrus <michael.ambrus@gmail.com>
2012-12-02 14:38:20 +0100

    overall: Cleaned up files from end white-spece
    Prof of concept for futil.onfile.sh. Command line used:
    
    src.shgrep.sh '[[:space:]]+$' | cut -f1 -d":" | sort -u | \
       futil.onfile.sh -v "sed -e 's/[[:space:]]\+$//'"
    
    This actually has an important and useful aspect, it makes new comit
    diffs less prone to diffs and merge conflicts.
    
    Category: cosmetic

--- openocd/install_and_setup.sh
+++ openocd/install_and_setup.sh
@@ -10,19 +10,19 @@ function install_and_setup() {
 	if [ $# -ge 1 ]; then
 		MAKEOPTS=$@
 	fi
-	
+
 	if [ -z "${MAKEOPTS}" ]; then
 		MAKEOPTS="-j$(cat /proc/cpuinfo | grep processor | wc -l)"
 	fi
-	
+
 	set -e
-	
+
 	source s3.user_response.sh
 	source s3.test_install_bin.sh
 	source s3.test_install_pkg.sh
-	
+
 	#set -u
-	
+
 	test_install_bin gcc
 	test_install_bin make
 	test_install_bin automake
@@ -31,17 +31,17 @@ function install_and_setup() {
 	test_install_bin aclocal
 	test_install_bin libtool
 	test_install_bin git
-	
+
 	test_install_pkg libftdi-dev
-	
+
 	FTDI_RULE='
 	BUS!="usb", ACTION!="add", SUBSYSTEM!=="usb_device", GOTO="kcontrol_rules_end"
-	
+
 	SYSFS{idProduct}=="0003", SYSFS{idVendor}=="15ba", MODE="664", GROUP="plugdev"
-	
+
 	LABEL="kcontrol_rules_end"
 	'
-	
+
 	if [ -z "$(grep -R 15ba /etc/udev/rules.d/*)" ]; then
 		set +e
 		ask_user_continue \
@@ -56,10 +56,10 @@ function install_and_setup() {
 				sudo tee /etc/udev/rules.d/45-ft2232.rules 1>/dev/null
 		fi
 	fi
-	
+
 	set -u
 	set -e
-	
+
 	mkdir OpenOCD
 	cd OpenOCD
 	git clone git://openocd.git.sourceforge.net/gitroot/openocd/openocd src
