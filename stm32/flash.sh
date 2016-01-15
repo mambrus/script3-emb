d9378cd4b5d8226ea90282a5d4226a8173ba810e
Michael Ambrus <michael.ambrus@gmail.com>
2016-01-15 19:44:56 +0100

    Distinguish between binary and other format files
    Needed as command sequence to openocd differ between bin-files and other
    (openocd cant figure it out by itself which is IMO an error).
    
    Change-Id: I3a2a7c7aa4bddc3e6b8e57b6cae1f24bc1a5808f

--- stm32/flash.sh
+++ stm32/flash.sh
@@ -10,7 +10,7 @@ function flash_bin() {
 	echo "Flashing binary"
 	emb.openocd.cmd.exp reset halt						&& \
 	emb.openocd.cmd.exp flash probe 0					&& \
-	emb.openocd.cmd.exp stm32x mass_erase 0				&& \
+	emb.openocd.cmd.exp stm32f1x mass_erase 0			&& \
 	emb.openocd.cmd.exp flash write_bank 0 $1 0			&& \
 	emb.openocd.cmd.exp reset halt						&& \
 	echo "Flashed [$1] OK"
@@ -21,6 +21,7 @@ function flash_form() {
 	echo "Flashing formatted"
 	emb.openocd.cmd.exp reset halt						&& \
 	emb.openocd.cmd.exp flash probe 0					&& \
+	emb.openocd.cmd.exp stm32f1x mass_erase 0			&& \
 	emb.openocd.cmd.exp flash write_image 1 1 $1 		&& \
 	emb.openocd.cmd.exp reset halt						&& \
 	echo "Flashed [$1] OK"
@@ -35,21 +36,35 @@ if [ "$FLASH_SH" == $( ebasename $0 ) ]; then
 	source s3.user_response.sh
 
 	#Several valid formats may be present. Next line chooses in preferred order
-	IMG=$( (ls *.bin 2>/dev/null; ls *.elf 2>/dev/null) | head -n1)
+
+	if [ $# -eq 1 ]; then
+		IMG=${1}
+	elif [ $# -eq 0 ]; then
+		#**Note** that the first one found is selected
+		IMG=$( (ls *.bin 2>/dev/null; ls *.elf 2>/dev/null) | head -n1)
+	else
+		echo "Err0r: Only one or zero arguments allowed" 1>&2
+		echo "(Zero for loading first *.bin found)" 1>&2
+		exit 1
+	fi
+
 	EXT=$(echo $IMG | sed -e 's/\(.*\)\.//')
 
-	if [ $# -ge 1 ]; then
-		#emb.openocd.cmd.exp $@
-		flash_form "$@"
-	else
+	if [ $# -eq 0 ] && [ "X${IMG}" != "X" ] ; then
 		ask_user_continue "Flash target with [${IMG}]? (Y/n)" || exit 1
-		if [ "X${EXT}" == "Xbin" ]; then
-			flash_bin $IMG
-			exit $?
-		else
-			flash_form $IMG
-			exit $?
-		fi
+	fi
+
+	if [ "X${IMG}" == "X" ] ; then
+		echo "Err0r: No flash-able file found or provided" 1>&2
+		exit 1
+	fi
+
+	if [ "X${EXT}" == "Xbin" ]; then
+		flash_bin $IMG
+		exit $?
+	else
+		flash_form $IMG
+		exit $?
 	fi
 fi
 
